9ec3d946e38b9698c41df2f45894073c5d89ca59
Ngô Nghĩa <ngonghiahp93@gmail.com>
2015-12-22 01:37:08 +0300

    Deleted events GameListener
    Changed events to PlayerActionListener

--- src/balda/model/events/GameListener.java
+++ src/balda/model/events/GameListener.java
@@ -10,8 +10,4 @@ public interface GameListener extends EventListener {
     public void gameFinished(GameEvent e);
     public void playerExchanged(GameEvent e);
     public void startingWordIsChoosen(GameEvent e);
-    public void playerScored(GameEvent e);
-    public void usedWord(GameEvent e);
-    public void nonexistentWord(GameEvent e);   
-    public void wordNotContainsCell(GameEvent e);
 }
