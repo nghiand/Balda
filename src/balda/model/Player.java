d10f260d0b50e1d086081b78453ead107a5139fc
Ngô Nghĩa <ngonghiahp93@gmail.com>
2015-12-27 13:43:37 +0300

    Update comment for javadoc class Player
    Update comment for javadoc class Player

--- src/balda/model/Player.java
+++ src/balda/model/Player.java
@@ -1,15 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package balda.model;
 
 /**
- *
- * @author Ngo Nghia
+ * Player
  */
 public class Player extends AbstractPlayer {
+    /**
+     * Constructor
+     * @param name name of player
+     * @param field game field
+     * @param database database dictionary
+     * @param used common dictionary, in which used words
+     */
     public Player(String name, GameField field, 
             Database database, UsedDictionary used){
         super(name, field, database, used);
