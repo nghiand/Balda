73c9fd6e69f2547d4a10207c851ec87c2db434c5
Ngô Nghĩa <ngonghiahp93@gmail.com>
2015-12-27 13:43:13 +0300

    Update comment for javadoc class Word
    Update comment for javadoc class Word

--- src/balda/model/Word.java
+++ src/balda/model/Word.java
@@ -2,18 +2,33 @@ package balda.model;
 
 import java.util.ArrayList;
 
+/**
+ * Word - List of chosen cells
+ */
 public class Word {
     String _word;
     ArrayList _listCells = new ArrayList<>();
     
+    /**
+     * Constructor
+     */
     public Word(){
         _word = "";
     }
     
+    /**
+     * Get length of word
+     * @return length
+     */
     public int getScore(){
         return _word.length();
     }
     
+    /**
+     * Check if Cell c is adjacent with last cell in word
+     * @param c cell
+     * @return true if cell c is adjacent with last cell in word
+     */
     public boolean isAdjacent(Cell c){
         if (_listCells.size() == 0) return true;
         Cell last = (Cell) _listCells.get(_listCells.size() - 1);
@@ -28,6 +43,11 @@ public class Word {
         return good;
     }
     
+    /**
+     * Check if word contains cell c
+     * @param c cells
+     * @return true if word contains cell c
+     */
     public boolean contains(Cell c){
         for (Object cell : _listCells){
             if (((Cell) cell).equals(c)) return true;
@@ -35,20 +55,36 @@ public class Word {
         return false;
     }
     
+    /**
+     * Add a cell to current word
+     * @param c cell
+     */
     public void addLetter(Cell c){
         _word += c.letter();
         _listCells.add(c);
     }
     
+    /**
+     * Remove all cell in word
+     */
     public void clear(){
         _word = "";
         _listCells.clear();
     }
     
+    /**
+     * Get current word
+     * @return word
+     */
     public String word(){
         return _word;
     }
     
+    /**
+     * Get cell in position
+     * @param i position
+     * @return cell
+     */
     Cell getCell(int i){
         if (i >= _listCells.size()) return null;
         return (Cell)_listCells.get(i);
