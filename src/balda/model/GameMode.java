0413aa72bfb9acdee75b727ccfc239038ff80cab
Ngô Nghĩa <ngonghiahp93@gmail.com>
2016-01-20 00:00:04 +0300

    Change class GameMode
    Change class GameMode

--- src/balda/model/GameMode.java
+++ src/balda/model/GameMode.java
@@ -1,4 +1,28 @@
 package balda.model;
 
-public enum GameMode {TWO_PLAYERS, EASY, NORMAL, HARD};
-
+/**
+ *
+ * @author Ngo Nghia
+ */
+public class GameMode {
+    private enum Mode {TWO_PLAYERS, VS_COMPUTER};
+    private Mode _mode;
+    private String _computerName;
+    
+    GameMode(){
+        _mode = Mode.TWO_PLAYERS;
+    }
+    
+    GameMode(String computerName){
+        _mode = Mode.VS_COMPUTER;
+        _computerName = computerName;
+    }
+    
+    boolean isTwoPlayers(){
+        return _mode == Mode.TWO_PLAYERS;
+    }
+    
+    String computerName(){
+        return _computerName;
+    }
+}
